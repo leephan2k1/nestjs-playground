f8c5b342e9b30241c9e6247201b85706f09be6af
leephan2k1 <pttl2k1@gmail.com>
2023-05-23 09:32:11 +0700

    feat: 🎸 apply express session

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { VersioningType } from '@nestjs/common';
+import * as session from 'express-session';
+import * as passport from 'passport';
 
 async function bootstrap() {
   const PORT = process.env.PORT ? parseInt(process.env.PORT) : 5001;
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('api');
+
+  app.use(
+    session({
+      secret: process.env.COOKIE_SECRET,
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+      },
+    }),
+  );
+
+  app.use(passport.initialize());
+  app.use(passport.session());
 
   app.enableVersioning({
     type: VersioningType.URI,
